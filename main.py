d36b57063e73204c78a9be68da786b119bd68e9f
Shane McDonough <shanemcdman@gmail.com>
2022-03-07 16:32:30 -0800

    Allow reading from file and interpreting interactive

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 
 from interpret import interpret
+from typing import IO
+from sys import argv
+
+def interactive():
+    try:
+        while 1:
+            interpret(input('interpreter> '))
+    except EOFError:
+        pass
+
+def from_file(file: IO[str]):
+    interpret(file.read())
 
 def main():
     '''Driver Code'''
-    interpret(
-        '(print \'Hello World!\' 100)'
-        '(print 1)'
-        '(print(add 1 2))'
-        '(print(add (mul 2.5 8) 20))'
-    )
+    _ = argv.pop(0)
+    match len(argv):
+        case 0:
+            interactive()
+        case 1:
+            with open(argv[0], 'r') as file:
+                from_file(file)
 
 if __name__ == '__main__':
     main()
